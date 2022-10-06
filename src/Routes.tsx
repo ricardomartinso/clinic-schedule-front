bb9d49b739e3535cbca056c22ea517e7edb5cccb
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-06 11:26:08 -0300

    feat: added routes to login, signup and home

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,3 +1,15 @@
+import { Routes as Router, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/SignUp";
+
 export function Routes() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Route path="/" element={<Login />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/home" element={<Home />} />
+    </Router>
+  );
 }

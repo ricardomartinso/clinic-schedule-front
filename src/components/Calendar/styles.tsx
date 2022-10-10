ff86b1f33454e4b9b6bcc9fc13f07290528af8ed
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-10 18:59:50 -0300

    refactor: added some responsivity changes to calendar

--- src/components/Calendar/styles.tsx
+++ src/components/Calendar/styles.tsx
@@ -8,18 +8,43 @@ export const CalendarStyled = styled(Calendar)`
   flex-direction: column;
   color: black;
   background-color: aliceblue;
-  border: 1px solid black;
+  border: 1px solid #ccc;
 
-  @media (max-width: 1200px) {
+  span {
+    font-size: 19px;
+  }
+
+  button {
+    font-size: 12.5px;
+  }
+  abbr {
+    font-size: 14px;
+  }
+
+  @media (max-width: 1250px) {
+    padding: 8px 6px;
     span {
-      font-size: 17px;
+      font-size: 17.5px;
     }
 
     button {
-      font-size: 13px;
+      font-size: 13.5px;
     }
     abbr {
-      font-size: 11.5px;
+      font-size: 13.1px;
+    }
+  }
+
+  @media (max-width: 900px) {
+    span {
+      font-size: 18.2px;
+    }
+
+    button {
+      font-size: 15.5px;
+    }
+    abbr {
+      font-size: 13.5px;
     }
   }
 `;

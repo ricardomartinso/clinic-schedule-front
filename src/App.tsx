36f82aef82104131262a308a78cdb2521c41044e
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-10 15:01:22 -0300

    feat: added calendar year to context

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,18 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   const [toShowDate, setToShowDate] = useState("");
   const [toGetDate, setToGetDate] = useState("");
+  const [year, setYear] = useState("");
 
   return (
     <DayContext.Provider
-      value={{ toShowDate, setToShowDate, toGetDate, setToGetDate }}
+      value={{
+        toShowDate,
+        setToShowDate,
+        toGetDate,
+        setToGetDate,
+        year,
+        setYear,
+      }}
     >
       <GlobalStyle />
       <Routes />

4aa6becf2b750e2be7f6460de6575f04717f3391
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-10 14:06:56 -0300

    feat: added toastify notifications on error

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ThreeDots } from "react-loader-spinner";
 import { api } from "../../services/api";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import {
   Container,
   Form,
@@ -11,21 +15,40 @@ import {
   Input,
   LoginError,
 } from "./styles";
+import { useUser } from "../../hooks/useUser";
 
 export function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [pwdError, setPwdError] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const { loginUser }: any = useUser();
+
+  const error = (message: string) =>
+    toast.error(`${message}`, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
 
   function submitForm(e: React.FormEvent) {
     e.preventDefault();
     setIsLoading(true);
 
     if (password.length < 6) {
+      setPwdError("true");
       setIsLoading(false);
-      return setIsError("Senha deve ter 6 caracteres de tamanho!");
+      error("Senha precisa ter pelo menos 6 caracteres");
+      setIsError(true);
+      return;
     }
 
     const loginInformation = {
@@ -35,13 +58,24 @@ export function Login() {
 
     api
       .post("/login", loginInformation)
-      .then(() => {
-        setIsLoading(false);
-        navigate("/home");
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response.data);
+          const { token } = response.data;
+
+          localStorage.setItem("token", token);
+          loginUser("User");
+          navigate("/home");
+        }
       })
       .catch((err) => {
         if (err.response.status === 401) {
-          setIsError("Email ou senha inválidos");
+          setPwdError("true");
+          setEmailError("true");
+          setEmail("");
+          setPassword("");
+          setIsError(true);
+          error("Email ou senha inválidos");
         }
         setIsLoading(false);
       });
@@ -51,28 +85,44 @@ export function Login() {
     <>
       <Container>
         <Logo>Clinic Scheduler</Logo>
-
         <Form onSubmit={submitForm}>
           <Input
             type="email"
             name="email"
-            disabled={isLoading}
-            required
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            color={emailError}
+            disabled={isLoading}
+            required
           />
           <Input
             type="password"
             name="password"
-            disabled={isLoading}
-            required
             placeholder="Senha"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            color={pwdError}
+            disabled={isLoading}
+            required
           />
 
-          {isError !== "" ? <LoginError>{isError}</LoginError> : ""}
+          {isError ? (
+            <ToastContainer
+              position="top-left"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              limit={4}
+            />
+          ) : (
+            ""
+          )}
 
           {isLoading ? (
             <Button type="submit" disabled={isLoading}>

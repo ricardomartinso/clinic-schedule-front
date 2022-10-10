89b2ba28e639e245cadfc1e3ed6e8ce73c2a324a
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-10 15:02:37 -0300

    feat: added login page responsivity

--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -2,11 +2,14 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Logo = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   font-family: "Saira Stencil One";
   font-weight: 400;
-  font-size: 32px;
+  font-size: 44px;
   line-height: 50px;
-  color: #ffffff;
+  color: white;
 `;
 
 export const Container = styled.div`
@@ -15,9 +18,11 @@ export const Container = styled.div`
   justify-content: center;
   flex-direction: column;
 
-  width: auto;
-  margin-inline: 24px;
+  width: 100%;
+
   min-height: 100vh;
+  background-color: #9e5767;
+  margin: 0 auto;
 `;
 
 export const Form = styled.form`
@@ -28,16 +33,41 @@ export const Form = styled.form`
   justify-content: flex-start;
   gap: 14px;
   margin-top: 32px;
+  padding: 0 4%;
+
+  @media (min-width: 768px) {
+    width: 66%;
+  }
+
+  @media (min-width: 1024px) {
+    width: 55%;
+  }
+
+  @media (min-width: 1444px) {
+    width: 45%;
+  }
+
+  @media (max-width: 768px) {
+    width: 85%;
+  }
+
+  @media (max-width: 540px) {
+    width: 100%;
+  }
 `;
+
 export const Input = styled.input`
   width: 100%;
   background: #ffffff;
+  border: ${(props) => (props.color !== "" ? "2px solid red" : "none")};
   border-radius: 5px;
   color: #000;
   font-size: 20px;
+
   line-height: 25px;
   padding: 12px 8px;
   outline: none;
+
   :disabled {
     background: #aaaaaa;
   }
@@ -54,7 +84,7 @@ export const Button = styled.button`
   height: 40px;
   border: none;
   border-radius: 9px;
-  background-color: #9e5767;
+  background-color: #2e72d8;
   color: white;
   font-size: 20px;
 `;
@@ -64,7 +94,7 @@ export const LinkStyled = styled(Link)`
   font-weight: 700;
   line-height: 18px;
   margin-top: 36px;
-  color: #fff;
+  color: white;
 `;
 
 export const LoginError = styled.span`

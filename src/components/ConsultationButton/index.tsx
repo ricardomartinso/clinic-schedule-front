98bfa70005a64f3a477fa59275834dbcf1bc3904
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-10 15:05:37 -0300

    feat: added ConsultationButton component and style

---
+++ src/components/ConsultationButton/index.tsx
@@ -0,0 +1,5 @@
+import { Link } from "./styles";
+
+export function ConsultationButton() {
+  return <Link to="/agendar">Agendar consulta</Link>;
+}
---
+++ src/components/ConsultationButton/styles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { Link as l } from "react-router-dom";
+
+export const Link = styled(l)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 190px;
+  height: 50px;
+  border: none;
+  background-color: #476f94;
+
+  font-size: 20px;
+  color: white;
+`;

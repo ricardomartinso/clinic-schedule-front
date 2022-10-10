1e28b7b351a59d6bb381f589866bcb1a9afdcc68
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-10 15:04:30 -0300

    feat: added future patients page

---
+++ src/pages/Patients/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Header";
+import { Container } from "./styles";
+
+export function Patients() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Em breve você poderá ver a lista de pacientes aqui!</h1>
+      </Container>
+    </>
+  );
+}
---
+++ src/pages/Patients/styles.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.main`
+  display: flex;
+
+  justify-content: flex-start;
+  flex-direction: column;
+
+  text-align: left;
+  padding: 2% 4%;
+  min-height: 93.7vh;
+  margin-top: 70px;
+
+  color: #333;
+
+  h1 {
+    font-size: 30px;
+    margin-top: 30px;
+  }
+`;

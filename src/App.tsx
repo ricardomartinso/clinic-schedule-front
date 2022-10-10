947ccd18eb72811a6f89afe6b9528fe097715559
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-10 14:06:01 -0300

    feat: added toastContainer and Day Context on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { GlobalStyle } from "./styles/globalStyle";
 import { Routes } from "./Routes";
+import { useContext, useState } from "react";
+import DayContext from "./contexts/DayContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [toShowDate, setToShowDate] = useState("");
+  const [toGetDate, setToGetDate] = useState("");
+
   return (
-    <div>
+    <DayContext.Provider
+      value={{ toShowDate, setToShowDate, toGetDate, setToGetDate }}
+    >
       <GlobalStyle />
       <Routes />
-    </div>
+      <ToastContainer />
+    </DayContext.Provider>
   );
 }
 
